d0ea3e12cb83215aef9c5aa197c009610e122e26
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-03 21:39:59 +0800

    chore: added aria-label on menu button for SEO accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,6 +15,7 @@ function Header() {
       <button
         className="size-6 hover:cursor-pointer"
         onClick={menuButtonClicked}
+        aria-label="Menu"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path
