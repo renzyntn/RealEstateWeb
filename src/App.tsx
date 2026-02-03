10250ff70b12c7fbf4589097481632ccd3e33aed
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-03 21:36:28 +0800

    chore: import route components to complete page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from "react-router";
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Listings from "./pages/Listings";
+import LetsMove from "./pages/LetsMove";
+import AboutUs from "./pages/AboutUs";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/letsmove" element={<LetsMove />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
