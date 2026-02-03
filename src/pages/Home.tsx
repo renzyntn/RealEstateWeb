4fff8e15bafc7a3f46b337bdd84dfa6124038f62
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-03 21:44:31 +0800

    chore: removed Header and Footer component and moved it in MainLayout.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Testimonial from "../components/Testimonial";
@@ -8,25 +7,20 @@ import Carousel from "../components/Carousel";
 import Services from "../components/Services";
 import Form from "../components/Form";
 import Location from "../components/Location";
-import Footer from "../components/Footer";
 
 function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Search />
-        <Testimonial />
-        <Carousel />
-        <Partners />
-        <Services />
-        <Form />
-        <Location />
-      </main>
-      <Footer />
-    </>
+    <main>
+      <Hero />
+      <About />
+      <Search />
+      <Testimonial />
+      <Carousel />
+      <Partners />
+      <Services />
+      <Form />
+      <Location />
+    </main>
   );
 }
 
