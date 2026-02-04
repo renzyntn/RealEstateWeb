3eb292ba8774409b094ee0dac7b68d554914707b
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-04 22:05:11 +0800

    chore: replace card parent flex-wrap instead of flex-row and gap-8 in md screens

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,13 +4,11 @@ function Services() {
   return (
     <section className="max-w-screen">
       <div className="w-full h-full flex flex-col justify-center items-center px-4 py-6 lg:p-8 text-black">
-        <div className="w-full flex flex-col items-center">
-          <h2 className="font-cinzel font-light text-3xl md:text-4xl mb-6 lg:mb-12">
-            OUR SERVICES
-          </h2>
-          <div className="grid grid-cols-1 md:w-full lg:w-5xl md:px-8 gap-6 md:gap-10 justify-center items-center font-montserrat">
-            <CardServices />
-          </div>
+        <h2 className="font-cinzel font-light text-3xl md:text-4xl mb-6 lg:mb-12">
+          OUR SERVICES
+        </h2>
+        <div className="flex flex-wrap md:px-8 gap-6 md:gap-8 justify-center items-center font-montserrat">
+          <CardServices />
         </div>
       </div>
     </section>
