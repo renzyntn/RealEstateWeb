be6e9aeeaa4832bc507af59e950f11291ada308b
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-04 17:52:16 +0800

    chore: map through carousel data in data.json to show image slides

--- src/components/ImageCarousel/Gallery.tsx
+++ src/components/ImageCarousel/Gallery.tsx
@@ -2,16 +2,22 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/swiper.css";
 import "swiper/swiper-bundle.css";
-
-import CarouselImg1 from "../../assets/images/carousel/carousel-image1.png";
-import CarouselImg2 from "../../assets/images/carousel/carousel-image2.png";
-import CarouselImg3 from "../../assets/images/carousel/carousel-image3.png";
-import CarouselImg4 from "../../assets/images/carousel/carousel-image4.png";
-import CarouselImg5 from "../../assets/images/carousel/carousel-image5.png";
-import CarouselImg6 from "../../assets/images/carousel/carousel-image6.png";
-import CarouselImg7 from "../../assets/images/carousel/carousel-image7.png";
+import carouselData from "../../data.json";
 
 function Gallery() {
+  const displaySlide = carouselData.carouselSection.map((data) => {
+    return (
+      <SwiperSlide>
+        <img
+          src={data.link}
+          alt={data.alt}
+          className="object-cover"
+          key={data.id}
+        />
+      </SwiperSlide>
+    );
+  });
+
   return (
     <Swiper
       modules={[Autoplay]}
@@ -21,55 +27,7 @@ function Gallery() {
       slidesPerView={1}
       spaceBetween={20}
     >
-      <SwiperSlide>
-        <img
-          src={CarouselImg1}
-          alt="Carousel Image 1"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg2}
-          alt="Carousel Image 2"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg3}
-          alt="Carousel Image 3"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg4}
-          alt="Carousel Image 4"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg5}
-          alt="Carousel Image 5"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg6}
-          alt="Carousel Image 6"
-          className="object-cover"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={CarouselImg7}
-          alt="Carousel Image 7"
-          className="object-cover"
-        />
-      </SwiperSlide>
+      {displaySlide}
     </Swiper>
   );
 }
