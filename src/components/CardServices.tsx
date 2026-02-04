7cee301d3d8fe45fc8425260de4f95e7a115828c
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-04 17:48:05 +0800

    chore: map through card data from data.json and add conditional rendering for card position

--- src/components/CardServices.tsx
+++ src/components/CardServices.tsx
@@ -1,69 +1,29 @@
-import servicesImg1 from "../assets/images/services/services-image1.png";
-import servicesImg2 from "../assets/images/services/services-image2.png";
-import servicesImg3 from "../assets/images/services/services-image3.png";
+import servicesData from "../data.json";
 
 function CardServices() {
-  return (
-    <>
-      <div className="card w-full md:w-lg bg-base-white shadow-xl/20 md:odd:justify-self-start">
+  const displayCard = servicesData.servicesSection.map((data, index) => {
+    return (
+      <div
+        className={`card w-full md:w-lg bg-base-white shadow-xl/20 md:odd:justify-self-start ${index % 2 === 0 ? "md:odd:justify-self-start" : "md:even:justify-self-end"}`}
+        key={data.id}
+      >
         <figure>
           <img
-            src={servicesImg1}
-            alt="Services Image 1"
+            src={data.link}
+            alt={data.alt}
             className="md:w-lg md:h-91 object-cover"
           />
         </figure>
         <div className="card-body space-y-2">
-          <h2 className="card-title text-xl md:text-2xl">
-            Real Estate Done Right
-          </h2>
+          <h2 className="card-title text-xl md:text-2xl">{data.title}</h2>
           <p className="text-base text-justify md:text-lg">
-            Nervous about your property adventure? Don’t be. Whether you're
-            getting ready to buy or sell your residence, looking at investment
-            properties, or just curious about the markets, our team ensures you
-            get the best experience possible!
+            {data.description}
           </p>
         </div>
       </div>
-      <div className="card w-full md:w-lg bg-base-white shadow-xl/20 md:even:justify-self-end">
-        <figure>
-          <img
-            src={servicesImg2}
-            alt="Services Image 2"
-            className="md:w-lg md:h-91 object-cover"
-          />
-        </figure>
-        <div className="card-body space-y-2">
-          <h2 className="card-title text-xl md:text-2xl">
-            Commercial and Residential
-          </h2>
-          <p className="text-base text-justify md:text-lg">
-            Large or small, condo or mansion, we can find it and get at the
-            price that's right. Fixer-uppers? Luxury? We can help with all of
-            it! We live, work, and play in this community. Happy to help you
-            find where to put you hard-earned dollars.
-          </p>
-        </div>
-      </div>
-      <div className="card w-full md:w-lg bg-base-white shadow-xl/20 md:odd:justify-self-start">
-        <figure>
-          <img
-            src={servicesImg3}
-            alt="Services Image 3"
-            className="md:w-lg md:h-91 object-cover"
-          />
-        </figure>
-        <div className="card-body space-y-2">
-          <h2 className="card-title text-xl md:text-2xl">Rely on Expertise</h2>
-          <p className="text-base text-justify md:text-lg">
-            If you have questions about affordability, credit, and loan options,
-            trust us to connect you with the right people to get the answers you
-            need in a timely fashion. We make sure you feel confident and
-            educated every step of the way.
-          </p>
-        </div>
-      </div>
-    </>
-  );
+    );
+  });
+
+  return <>{displayCard}</>;
 }
 export default CardServices;
