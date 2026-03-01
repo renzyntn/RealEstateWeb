a08b0f0004215d4e8f1f7d1d2e04d6e43c1bfb93
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-03-01 16:44:48 +0800

    chore: remove h-200 when in lg screen

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -2,15 +2,13 @@ import locationData from "../data.json";
 
 function Location() {
   return (
-    <section className="max-w-screen">
-      <div className="w-full h-60 lg:h-120">
-        <iframe
-          src={locationData.locationSection.mapLink}
-          title={locationData.locationSection.title}
-          className="w-full h-full object-cover"
-        />
-      </div>
-    </section>
+    <div className="w-full h-60">
+      <iframe
+        src={locationData.locationSection.mapLink}
+        title={locationData.locationSection.title}
+        className="w-full h-full object-cover rounded-lg"
+      />
+    </div>
   );
 }
 export default Location;
